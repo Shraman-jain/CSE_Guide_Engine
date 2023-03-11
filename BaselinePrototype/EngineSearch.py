bc4796f6910960acc6daa88b9e77cc578ea8b4f1
Shraman-jain <60072287+Shraman-jain@users.noreply.github.com>
2023-03-12 00:26:36 +0530

    Add files via upload

---
+++ BaselinePrototype/EngineSearch.py
@@ -0,0 +1,57 @@
+import requests
+from requests.exceptions import RequestException
+import pandas as pd
+from DataStorage import DBStorage
+from datetime import datetime 
+from urllib.parse import quote_plus
+
+def search_api(query, pages=int(10/10)):
+    results = []
+    searchURL="https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl="+"in"
+    for i in range(0, pages):
+        start = i*10+1
+        url = searchURL.format(
+            key="AIzaSyDbDg1UgkcHzKvZ8Otq4RIgsm-nShyZggM",
+            cx="04e272a77e86e4b45",
+            query=quote_plus(query),
+            start=start
+        )
+        response = requests.get(url)
+        data = response.json()
+        results += data["items"]
+    res_df = pd.DataFrame.from_dict(results)
+    res_df["rank"] = list(range(1, res_df.shape[0] + 1))
+    res_df = res_df[["link", "rank", "snippet", "title"]]
+    return res_df
+
+def scrape_page(links):
+    html = []
+    for link in links:
+        print(link)
+        try:
+            data = requests.get(link, timeout=5)
+            html.append(data.text)
+        except RequestException:
+            html.append("")
+    return html
+
+def search(query):
+    columns = ["query", "rank", "link", "title", "snippet", "html", "created"]
+    storage = DBStorage()
+
+    stored_results = storage.query_results(query)
+    if stored_results.shape[0] > 0:
+        stored_results["created"] = pd.to_datetime(stored_results["created"])
+        return stored_results[columns]
+
+    print("No results in database.  Using the API.")
+    results = search_api(query)
+    html = scrape_page(results["link"])
+    results["html"] = html
+    results = results[results["html"].str.len() > 0].copy()
+    results["query"] = query
+    results["created"] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    results = results[columns]
+    results.apply(lambda x: storage.insert_row(x), axis=1)
+    print(f"Inserted {results.shape[0]} records.")
+    return results 
