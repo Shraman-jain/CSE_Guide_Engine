827196b947bf4db9e034bc8a36380ad4579d1884
swatantraiiitd <125566784+swatantraiiitd@users.noreply.github.com>
2023-04-23 22:47:42 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,5 @@
+SEARCH_KEY = "AIzaSyADPlkRdBAJDULDFQXSx4gBD8CDRjNPNtA"
+SEARCH_ID = "04e272a77e86e4b45"
+COUNTRY = "in"
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
+RESULT_COUNT = 11
